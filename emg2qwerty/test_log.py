af36370479c14c1ed5d5b1335e45ff78f26771d1
SamuelLo <samuello300@gmail.com>
2026-03-12 12:47:31 -0700

    seeing if this prints out new lines or not

--- emg2qwerty/test_log.py
+++ emg2qwerty/test_log.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
 
     log_data = [80.0, 75.0, 70.0, 65.0, 60.0, 55.0, 50.0, 45.0, 40.0, 35.0]
 
-    print("CER History:", json.dumps(log_data, indent=4))
+    print("CER History:", json.dumps(log_data))
 
