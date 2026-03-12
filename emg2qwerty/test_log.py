a51989f32135d016c3cbc1c8533cfd31300ad210
SamuelLo <samuello300@gmail.com>
2026-03-12 12:38:44 -0700

    updating print

--- emg2qwerty/test_log.py
+++ emg2qwerty/test_log.py
@@ -3,6 +3,7 @@ TODO:
 - Output a dictionary of mock log data
 - Get the output after each epoch for the lightning.py
 
+- if I maintain metrics in a different python file and allow that python file to record into a log 
 """
 import pprint
 
@@ -11,5 +12,5 @@ if __name__ == "__main__":
 
     log_data = [80.0, 75.0, 70.0, 65.0, 60.0, 55.0, 50.0, 45.0, 40.0, 35.0]
 
-    pprint.pprint("CER History:", json.dumps(log_data, indent=4))
+    print("CER History:", json.dumps(log_data, indent=4))
 
